235a7f28fecc497546cadbcca763c7da5b47c40d
rgoodman22 <robertgoodman2022@u.northwestern.edu>
2021-01-09 02:21:47 -0500

    modularization

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { SafeAreaView, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { StyleSheet, SafeAreaView } from 'react-native';
+import Banner from './components/Banner';
+import CourseList from './components/CourseList';
 
 const schedule = {
   "title": "CS Courses for 2018-2019",
@@ -27,30 +29,6 @@ const schedule = {
   ]
 };
 
-const getCourseNumber = course => (
-  course.id.slice(1)
-);
-
-const Course = ({ course }) => (
-  <TouchableOpacity style={styles.courseButton}>
-    <Text style={styles.courseText}>
-      {`CS ${getCourseNumber(course)}\n${course.meets}`}
-    </Text>
-  </TouchableOpacity>
-);
-
-const CourseList = ({ courses }) => (
-  <ScrollView>
-    <View style={styles.courseList}>
-      { courses.map(course => <Course key={course.id} course={ course } />) }
-    </View>
-  </ScrollView>
-);
-
-const Banner = ({ title }) => (
-  <Text style={styles.banner}>{ title }</Text>
-);
-
 const App = () => {
   return (
     <SafeAreaView style={styles.container}>
@@ -58,41 +36,14 @@ const App = () => {
       <CourseList courses={schedule.courses} />
     </SafeAreaView>
   );
-};
-  
+}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
     paddingTop: 20,
-  },
-  banner: {
-    color: '#888',
-    fontSize: 32,
-  },
-  courseList: {
-    flex: 1,
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
-  },
-  courseButton: {
-    borderRadius: 5,
-    justifyContent: 'center',
-    alignItems: 'center',
-    margin: 10,
-    height: 60,
-    padding: 10,
-    minWidth: 90,
-    maxWidth: 90,
-    backgroundColor: '#66b0ff',
-  },
-  courseText:{
-    color: '#fff',
-    fontSize: 12,
-    textAlign: 'center',
   },
 });
 
