9be7577cbfc3a1e017570843804c864359057daa
rgoodman22 <robertgoodman2022@u.northwestern.edu>
2021-01-09 01:43:05 -0500

    Course List

--- App.js
+++ App.js
@@ -1,15 +1,40 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, SafeAreaView } from 'react-native';
+import Banner from './components/Banner';
+import CourseList from './components/CourseList';
 
 const schedule = {
-  title: "CS Courses for 2018-2019"
+  "title": "CS Courses for 2018-2019",
+  "courses": [
+    {
+      "id": "F101",
+      "title": "Computer Science: Concepts, Philosophy, and Connections",
+      "meets": "MWF 11:00-11:50"
+    },
+    {
+      "id": "F110",
+      "title": "Intro Programming for non-majors",
+      "meets": "MWF 10:00-10:50"
+    },
+    {
+      "id": "F111",
+      "title": "Fundamentals of Computer Programming I",
+      "meets": "MWF 13:00-13:50"
+    },
+    {
+      "id": "F211",
+      "title": "Fundamentals of Computer Programming II",
+      "meets": "TuTh 12:30-13:50"
+    }
+  ]
 };
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.bannerStyle}>{schedule.title}</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Banner title={schedule.title} />
+      <CourseList courses={schedule.courses} />
+    </SafeAreaView>
   );
 }
 
@@ -18,11 +43,8 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: 20,
   },
-  bannerStyle: {
-    color: '#888',
-    fontSize: 32,
-  }
 });
 
 export default App;
--- Notes.md
+++ Notes.md
@@ -2,8 +2,9 @@
 
 ## Resources
 
-1. [Getting Started ](https://reactnative.dev/docs/getting-started)
-2. [React Native Styling Cheatsheet ](https://github.com/vhpoet/react-native-styling-cheat-sheet)
-3. [A year of React Native styling](https://www.madebymany.com/stories/a-year-of-react-native-styling-part-1)
-4. [Design System for Dummies: Create your own flavor of React Native app in 3 Easy Steps](https://medium.com/wix-engineering/design-system-for-dummies-create-your-own-flavor-of-react-native-app-in-3-easy-steps-f49dc095b513)
-5. [The Iceberg of React Hooks](https://medium.com/@sdolidze/the-iceberg-of-react-hooks-af0b588f43fb)
+1. [In-depth styling with React Native and Flexbox](https://freecontent.manning.com/in-depth-styling-with-react-native-and-flexbox/).
+2. [Getting Started ](https://reactnative.dev/docs/getting-started)
+3. [React Native Styling Cheatsheet ](https://github.com/vhpoet/react-native-styling-cheat-sheet)
+4. [A year of React Native styling](https://www.madebymany.com/stories/a-year-of-react-native-styling-part-1)
+5. [Design System for Dummies: Create your own flavor of React Native app in 3 Easy Steps](https://medium.com/wix-engineering/design-system-for-dummies-create-your-own-flavor-of-react-native-app-in-3-easy-steps-f49dc095b513)
+6. [The Iceberg of React Hooks](https://medium.com/@sdolidze/the-iceberg-of-react-hooks-af0b588f43fb)
